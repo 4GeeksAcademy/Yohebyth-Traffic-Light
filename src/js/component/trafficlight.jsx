37d5bdf83131810f107ddb5b0a947defad945083
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-11 21:56:28 +0000

    paint

---
+++ src/js/component/trafficlight.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TrafficLight = () =>{
+    return(
+        <main>
+            <div className="stick">
+
+            </div>
+            <div className="lightContent">
+                <div className="redLight"></div>
+                <div className="yellowLight"></div>
+                <div className="greenLight"></div>
+            </div>
+        </main>
+    )
+}
+
+export default TrafficLight;
